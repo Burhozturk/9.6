89a50f1ceb1e3eff71e94a3326b9f9cfb906d902
Burhan Öztürk <burh0125@edu.easj.dk>
2018-11-01 19:41:27 +0100

    finished

---
+++ src/StopWatch.java
@@ -0,0 +1,27 @@
+public class StopWatch
+{
+    private long startTime;
+    private long endTime;
+
+    public StopWatch()
+    {
+        this.startTime=System.currentTimeMillis();
+    }
+
+    public void start()
+    {
+        this.startTime=System.currentTimeMillis();
+
+    }
+
+    public void stop()
+    {
+        this.endTime=System.currentTimeMillis();
+    }
+
+    public void getElapsedTime()
+    {
+        System.out.println(System.nanoTime());
+    }
+
+}
